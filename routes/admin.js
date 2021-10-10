a0db09166d8ef1695a52aee9f01c9e1f905bcc8a
fanyCaz <stepheniecaz@gmail.com>
2021-10-10 17:26:03 -0500

    login started

---
+++ routes/admin.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req,res,next){
+  console.log("res");
+});
+
+router.get('/login', function(req,res,next){
+  res.render('login');
+});
+
+module.exports = router;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    colors: {
+      normal: '#efe2e0',
+    },
+  },
+  variants: {},
+  plugins: [],
+}
