0356230c99fe87f30f7bb0b566dabfebaa0af357
fanyCaz <stepheniecaz@gmail.com>
2021-11-10 23:02:20 -0600

    send available appoitnments to client

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const appointmentController = require('../controllers/appointmentController');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home' });
 });
 
-router.get('/calendar', function(req, res, next) {
-  res.render('client_calendar');
-});
+router.get('/calendar', appointmentController.getPublicAppointments);
 
 module.exports = router;
