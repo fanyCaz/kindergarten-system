71fbb8e34464588417eca7c66dab56d7dfb9a2cf
Eduardo Salazar <eduardosalaz@outlook.com>
2021-11-21 15:52:08 -0600

    Testing pero no funcionan muy bien

--- tests/admin.test.js
+++ tests/admin.test.js
@@ -1,5 +1,6 @@
 const sum = require('../public/javascripts/sum');
 const request = require("supertest");
+const server = request.agent('http://localhost:3000');
 const app = require('../app');
 
 
@@ -13,11 +14,70 @@ describe("Testear la ruta principal", () => {
   });
 });
 
-/*
-test('adds 1 + 1 equals 2', () =>{
-  expect(sum(1,1)).toBe(2);
+describe("Testear autentificacion", () => {
+  test("Debe de responder con un 302 de FOUND, para redirigir a login", () => {
+    return request(app)
+        .get("/admin/schedule")
+        .then(response => {
+          expect(response.statusCode).toBe(302);
+        });
+  });
 });
-*/
+
+
+describe('GET /nino', function(){
+    it('login', loginUser());
+    it('uri that requires user to be logged in', function(done){
+    server
+        .get('/info-nino/1')                       
+        .expect('Content-Type', /json/)
+	.expect(200)
+        .end(function(err, res){
+            if (err) return done(err);
+            console.log(res.body);
+            done()
+        });
+    });
+});
+
+describe('POST /add-child', function(){
+    it('login', loginUser());
+    it('uri that requires user to be logged in', function(done){
+    server
+        .post('/add-child')     
+	.send({'firstName':"Lalo"})                  
+        .set('Accept', 'application/json')
+      	.expect('Content-Type', /json/)
+	.expect(200)
+        .end(function(err, res){
+            if (err) return done(err);
+            console.log(res.body);
+            done()
+        });
+    });
+});
+
+
+
+
+
+
+
+function loginUser() {
+    return function(done) {
+        server
+            .post('/api/auth')
+            .send({ username: 'example@example.com', password: 'password' })
+            .expect(302)
+            .expect('Location', '/admin/schedule')
+            .end(onResponse);
+
+        function onResponse(err, res) {
+           if (err) return done(err);
+           return done();
+        }
+    };
+};
 
 
 
